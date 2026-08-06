f5239e6b6bd9719685dca34af1d8f40098349036
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-08-06 13:47:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket        = "my-terraform-state-bucket"
     key           = "iacm/terraform.tfstate"
-    region        = "us-east-1"
+    region        = "eu-north-1"
     encrypt       = true
   }
 }
