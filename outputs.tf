3f8dcff80ee9b27d71cfdf49c916df66d27815b8
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-08-21 15:07:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,16 @@
-output "bucket_id" {
+output "bucket_name" {
   description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.state_bucket.id
+  value       = aws_s3_bucket.state_bucket.bucket
 }
 
 output "bucket_arn" {
   description = "The ARN of the S3 bucket"
   value       = aws_s3_bucket.state_bucket.arn
+}
+
+output "bucket_domain_name" {
+  description = "The bucket domain name (for access endpoints)"
+  value       = aws_s3_bucket.state_bucket.bucket_domain_name
 }
 
 output "bucket_region" {
