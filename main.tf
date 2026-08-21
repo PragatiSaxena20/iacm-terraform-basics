8d688140438a57ddfd15b82c5b445116779bc8f2
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-08-21 15:05:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource : "aws_s3_bucket" "demo_bucket" {
+resource : "aws_s3_bucket" "state_bucket" {
   bucket = var.bucket_name
   tags = var.tags
 }
